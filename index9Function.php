2ccdc6e22eb3458a158098ed3c0314c9bf87ffb1
Oosher <105375761+Oosher@users.noreply.github.com>
2023-10-03 05:08:09 +0300

    Update index9Function.php

--- index9Function.php
+++ index9Function.php
@@ -1,29 +1,5 @@
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=
-    
-    , initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="index.php" method="post">
-        <label>User name:</label>
-        <input type="text" name="username"><br>
-        <label>password:</label>
-        <input type="password" name="password"><br>
-        <input type="submit">
-    </form>
-</body>
-</html>
-
-
-
-
-
 <?php
 
     function firstFuncInPhp(int $a ,int $b) {
